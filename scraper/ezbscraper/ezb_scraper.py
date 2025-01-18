86ff8664627eaa66e34929e47baa443adbf30a63
Mahmoud Rafatii <rafati@outlook.com>
2025-01-18 13:30:44 +0100

    added method for ecb bullets (not finished)

--- scraper/ezbscraper/ezb_scraper.py
+++ scraper/ezbscraper/ezb_scraper.py
@@ -1,43 +1,117 @@
 # import pandas , bs4 and requests 
+import re
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import requests as req
 import pandas as pd
-import time 
+import time
+from tqdm import tqdm 
 
 # Url of the main EZB Monetary Policy page
-url = 'https://www.ecb.europa.eu/press/govcdec/mopo/html/index.en.html'
+MPD_URL = 'https://www.ecb.europa.eu/press/govcdec/mopo/html/index.en.html'
+EEB_URL = 'https://www.ecb.europa.eu/press/economic-bulletin/html/all_releases.en.html'
+BROWSER = webdriver.Chrome()
 
-chrome = webdriver.Chrome()
-chrome.get(url)
-elem = chrome.find_element(By.TAG_NAME,"html")
-elem.send_keys(Keys.END)
-time.sleep(3)
+def clean_text(text):
+    text_no_newlines = re.sub(r'[\n\r\t]+', ' ', text)
+    text= re.sub(r'[\s;]+', ' ', text_no_newlines).strip()
+
+    return text
+
+def seleniumstarter(url, chrome = BROWSER):
+    chrome.get(url)
+    elem = chrome.find_element(By.TAG_NAME,"html")
+    elem.send_keys(Keys.END)
+    time.sleep(3)
+    elem.send_keys(Keys.HOME)
+
+    source = chrome.page_source
+    return source
+
+def get_bullet_links(source):
+    soup = BeautifulSoup(source, 'html.parser')
+    dl = soup.find('dl')
+
+    links = {}
+    for child in dl:
+        if child.name == 'dt':
+            key = child['isodate']
+            value = child.next_sibling.find('div', {'class':'title'}).a['href']
+            links[key] = 'https://www.ecb.europa.eu/' + value   
+    return links
+
+def get_linkdict(source):
+    soup = BeautifulSoup(source, 'html.parser')
+    divs = soup.find_all('div', {'class' : 'lazy-load loaded'})
+    links = {}
+
+    for div in divs:
+        for conts in div.contents:
+            if conts == '\n':
+                continue
+            if conts.name == 'dt':
+                key = conts['isodate']
+                dd_sibling = conts.next_sibling
+                if dd_sibling.name == 'dd' and 'href' in dd_sibling.contents[0].next.attrs.keys():
+                    value = dd_sibling.contents[0].next['href']
+                links[key] = 'https://www.ecb.europa.eu/' + value
+            else:
+                continue
+    return links
+
+def download_reports(links):
+    reports = {}
+    for date, link in tqdm(links.items(), desc= "Downloading reports"):
+        try:
+            res = req.get(link)
+            text = res.text
+            soup = BeautifulSoup(text, 'html.parser')
+            main = soup.main.extract()
+            report = ''
+            for conts in main:
+                try: 
+                    if conts.DEFAULT_INTERESTING_STRING_TYPES:
+                        if 'class' in conts.attrs and 'section' in conts['class'] :
+                            report += conts.text
+                except Exception as e:
+                    continue
+            reports[date] = report
+        except Exception as e:
+            print(e)
+            continue
+    return reports
 
 
-elem.send_keys(Keys.HOME)
+def save_reports(reports, output = 'reports.csv'):
+    df = pd.DataFrame(reports.items(), columns = ['Date', 'Report'])
+    df.insert(1, 'Type', 'Monetary Policy Decisions')
+    df['Report'] = df['Report'].apply(clean_text)
+    df.to_csv(output, index = False)
 
-sorce = chrome.page_source
-soup = BeautifulSoup(sorce, 'html.parser')
-div = soup.find('div', {'class' : 'lazy-load loaded'})
-# for all the dt and dd children in div pair always them pairwise
-dts = div.find('dt', recursive=False)
-dds = div.find('dd', recursive=False)
-print(dts)
-print(dds)
-isodates = []
-for date in dts:
-    isodates.append(date.get('isodate'))
-print(isodates)
-links = []
-for link in dds:
-    a_tags = link.find_all('a')
-    for a_tag in a_tags:
-        if a_tag.get('lang') =='en':
-            href = a_tag.get('href')
-            links.append(href)
-dateLinkDict = {}
-for date, link in enumerate(isodates, links):
-    dateLinkDict[date] = link
+        
+
+
+def main():
+    url = EEB_URL
+    start_time = time.time()
+    print("Starting EZB scraping process...")
+
+    output_path_mpd = 'data/raw_pdf/ezb/all_reports.csv'
+    output_path_ebb = 'data/raw_pdf/ezb/all_bulletins.csv'
+
+    source = seleniumstarter(url)
+    bullet_links = get_bullet_links(source)
+    #links = get_linkdict(source)
+    reports = download_reports(bullet_links)
+    save_reports(reports, output_path)
+
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"\nTotal execution time: {execution_time:.2f} seconds")
+    print(f"Downloaded {len(reports)} reports")
+
+if __name__ == '__main__':
+    main()
