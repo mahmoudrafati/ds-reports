ec231318f9cc5edd316f81de45b5793b0bc0e178
Mahmoud Rafatii <rafati@outlook.com>
2025-01-22 18:34:51 +0100

    renaming

--- scraper/util/ezb_rename.py
+++ scraper/util/ezb_rename.py
@@ -8,6 +8,7 @@ for year in os.listdir(base_dir):
     if os.path.isdir(os.path.join(base_dir, year)):
         for file in os.listdir(os.path.join(base_dir, year)):
             if file.endswith('.csv'):
+                df = pd.read_csv(os.path.join(base_dir, year, file), sep=';')
                 monthname = pd.to_datetime(df['Date'], errors='raise').dt.strftime('%B').iloc[0] + '.csv'
                 os.rename(os.path.join(base_dir, year, file), os.path.join(base_dir, year, monthname))
             else:
